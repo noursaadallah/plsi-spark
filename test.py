1ea96d635f79615f6fffccef661c620ff4f84bb9
noursaadallah <nour.saadallah@gmail.com>
2018-12-08 13:57:27 +0100

    likelihood 1st step

--- test.py
+++ test.py
@@ -12,14 +12,17 @@ def addZ (us):
     return (usz)
 
 sc = pyspark.SparkContext()
+likelihood = []
 
 ratingsCSV = sc.textFile("file:///home/noursaadallah/Desktop/big-data/ml-latest-small/ratings.csv")
 header = ratingsCSV.first()
 # remove header:
 ratings = ratingsCSV.filter(lambda x : x != header)
 # keep only userId and movieId columns: (u,s)
-ratings = ratings.map(lambda x : x.split(',')[0]+','+x.split(',')[1])
+ratings = ratings.map(lambda x : x.split(',')[0]+','+x.split(',')[1]).persist()
 
+_N = sc.broadcast(ratings.count() )
+N = _N.value
 # define number of clusters=latent classes
 nb_zBC = sc.broadcast(3)
 nb_z = nb_zBC.value
@@ -57,11 +60,20 @@ zu = zu.map(lambda x : (x[0],x[1]/sum_zu) ).persist()
 ############################# EM #############################
 # first iteration
 # compute p(s|u) = Sum over z of (p(z|u) p(s|z)) 
-# su : (s,u), p(s|u)
-sz_z = sz.map(lambda x : ( x[0].split(',')[1] , x[0].split(',')[0]+','+str(x[1]))).persist()
-zu_z = zu.map(lambda x : ( x[0].split(',')[0] , x[0].split(',')[1]+','+str(x[1]))).persist()
-
-z_sz_zu = sz_z.join(zu_z)
-print z_sz_zu.collect()[0:5]
-
+# sz_zu : (s,z,p(s|z)), (z,u,p(z|u))
+cart_prod = sz.cartesian(zu)
+sz_zu = cart_prod.filter(lambda x : x[0][0].split(',')[1] == x[1][0].split(',')[0])\
+    .map(lambda x : ( x[0][0].split(',')[0]+','+x[1][0].split(',')[1] , x[0][1]*x[1][1] ) )
+# su : s,u p(s|u)=sum over z of p(s|z)*p(z|u)
+su = sz_zu.reduceByKey(lambda x,y : x+y).persist()
 # compute objective function (log-likelihood) : L = 1/N * sum over n of log(p(s|u))
+_ratings = ratings.map(lambda x : (x.split(',')[1]+','+x.split(',')[0] , 0))
+
+Lset = _ratings.join(su)
+
+L = -Lset.map(lambda x: np.log(x[1][1])).reduce(lambda x,y : x+y)/Lset.count()
+print L
+likelihood.append(L)
+
+#EM Algorithm 
+#E Step: q* = (p(s/z)p(z/u))/Sum over z p(s/z)p(z/u)
